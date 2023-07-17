70741120e444b9d9802fa9ebf4442410bba03a2b
kwangho <pokljh0411@gmail.com>
2023-07-17 13:14:50 +0900

    Pin상세페이지 기능추가

--- core/pins/urls.py
+++ core/pins/urls.py
@@ -6,4 +6,5 @@ app_name = 'pins'
 urlpatterns = [
     path('list', PinListView.as_view(), name='list'),
     path('create', PinCreateView.as_view(), name='create'),
+    path('detail/<int:pin_id>', PinDetailView.as_view(), name='detail'),
 ]
--- core/pins/views.py
+++ core/pins/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
@@ -49,3 +49,18 @@ class PinCreateView(LoginRequiredMixin, View):
         
         return render(request, 'pins/create.html', context=context)
 
+
+### PinDetail
+class PinDetailView(View):
+    # Pin상세 페이지
+    def get(self, request, pin_id):
+        pin = get_object_or_404(Pin, pk=pin_id)
+        context = {
+            'pin_writer_id': pin.writer.pk,
+            'pin_image_url': pin.image.url,
+            'pin_title': pin.title,
+            'pin_content': pin.content,
+            'pin_like_count': pin.like_count
+        }
+        
+        return render(request, 'pins/detail.html', context=context)
