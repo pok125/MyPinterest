91db7dd133b850b79cb0f6f407b0b0b49e0db2b5
kwangho <pokljh0411@gmail.com>
2024-12-05 16:11:45 +0900

    profile을 user에 합치는 작업

--- core/users/urls.py
+++ core/users/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from users.views import *
 
-app_name = 'users'
+app_name = "users"
 
 urlpatterns = [
-    path('join/', JoinView.as_view(), name='join'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('delete/<int:user_id>', UserDeleteView.as_view(), name='delete'),
-    path('following/<int:user_id>', FollowingView.as_view(), name='following'),
-    path('unfollowing/<int:user_id>', UnFollowingView.as_view(), name='unfollowing'),
+    path("join/", JoinView.as_view(), name="join"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("delete/<int:user_id>", UserDeleteView.as_view(), name="delete"),
+    path("following/<int:user_id>", FollowingView.as_view(), name="following"),
+    path("unfollowing/<int:user_id>", UnFollowingView.as_view(), name="unfollowing"),
+    path("mypage/<int:user_id>/", MyPageView.as_view(), name="mypage"),
+    path("update/<int:user_id>/", MyPageUpdateView.as_view(), name="update"),
 ]
